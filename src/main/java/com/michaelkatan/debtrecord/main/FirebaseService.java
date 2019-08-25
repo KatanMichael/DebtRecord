892b008082116b4f25b14ca092ea17dd47a2a105
Michael Katan <michaelkatan@gmail.com>
2019-08-25 20:02:40 +0300

    firebase added

--- src/main/java/com/michaelkatan/debtrecord/main/FirebaseService.java
+++ src/main/java/com/michaelkatan/debtrecord/main/FirebaseService.java
@@ -1,21 +1,60 @@
 package com.michaelkatan.debtrecord.main;
 
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.michaelkatan.debtrecord.interfaces.DAO;
 import com.michaelkatan.debtrecord.models.Debt;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Repository
 public class FirebaseService implements DAO
 {
+    FirebaseApp firebaseApp;
+
+    public FirebaseService()
+    {
+        initFirebase();
+    }
+
+    private void initFirebase()
+    {
+
+
+            FirebaseOptions options = null;
+            try {
+                InputStream stream = this.getClass().getResourceAsStream("/debtrecord.json");
+                options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(stream))
+                        .setDatabaseUrl("https://triviaapi-d1d92.firebaseio.com")
+                        .build();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            firebaseApp = null;
+            List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+            if(firebaseApps!=null && !firebaseApps.isEmpty()){
+                for(FirebaseApp app : firebaseApps){
+                    if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
+                        firebaseApp = app;
+                }
+            }
+            else
+                firebaseApp = FirebaseApp.initializeApp(options);
+        }
+
+
 
     @Override
     public void addDebt(Debt debt)
     {
-
 
     }
 
